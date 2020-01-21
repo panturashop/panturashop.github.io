5ba91d9d5b909d873272152d0141f673d880abe9
panturashop <60152143+panturashop@users.noreply.github.com>
2020-01-22 01:20:13 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Pantura Shop</title>
+
+</head>
+<body>
+
+<form action="proses.php" method="get">
+
+      <p>Jenis Transaksi
+
+	  <select name='Jenis Transaksi'>
+
+		<option value='Penjualan'>Penjualan</option>
+
+		<option value='Pengeluaran'>Pengeluaran</option>
+		
+		<option value='Restock GPC'>Restock GPC</option>
+
+		<option value='Restock IGC'>IGC</option>
+		
+		<option value='Restock GB'>GB</option>
+
+	  </select>
+	
+
+	  </p>
+	<p>
+	<select>
+		<option>opsi no 1</option>
+		<option>opai no 2</option>
+	</select>
+	
+	</p>
+
+<label>Nominal</label>
+<input type="text"  name="nominal"/>
+<input type='submit' name='tombol' value='submit' />
+
+</form>
+
+
+</body>
+</html>
